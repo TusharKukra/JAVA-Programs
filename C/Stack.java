81c112f418896986b7638b325c8fccbc3c87bc24
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2022-01-06 18:26:00 +0530

    Create Stack.java

---
+++ C/Stack.java
@@ -0,0 +1,47 @@
+package learn;
+
+import java.util.*;
+
+public class StackCollection {
+	
+	// Based on Last-In First-Out Principle
+	
+	public static void main(String[] args) {
+		
+		Stack<String> animals = new Stack<>();
+		
+		animals.push("Tiger");
+		animals.push("Lion");
+		animals.push("Zebra");
+		animals.push("Elephant");
+		
+		System.out.println("Stack: "+animals);
+		
+		// Peek: To check which is the topmost element (the last element entered will be on the top)
+		System.out.println("Peek: "+animals.peek());
+		
+		// Pop: to remove/delete element of top most position
+		animals.pop();
+		System.out.println("After Pop: "+animals);
+		
+		// Iterator:
+		
+		// for each loop
+		for(String str: animals) {
+			System.out.println(str);
+		}
+		
+		// Iterator Class
+		Iterator<String> str = animals.iterator();
+		while(str.hasNext()) {
+			System.out.println(str.next());
+		}
+		
+		// forEach method
+		animals.forEach(e->{
+			System.out.println(e);
+		});
+		
+	}
+	
+}
