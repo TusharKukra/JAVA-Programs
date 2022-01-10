04bf1d30780216edc042f595ec3394ce4afae31f
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2022-01-10 11:26:16 +0530

    Create HashMapClass.java

---
+++ C/Map/HashMapClass.java
@@ -0,0 +1,36 @@
+package learn;
+
+import java.util.*;
+
+public class MapClassss {
+
+		
+	public static void main(String[] args) {
+		
+		// HashMap
+		HashMap<String, Integer> hm = new HashMap<>();
+		
+		// adding Elements
+		hm.put("ABC", 1);
+		hm.put("XYZ", 2);
+		hm.put("GHI", 3);
+		hm.put("LMN", 4);
+		 
+		hm.put("LMN", 4); // Duplicates are not allowed in HashMap
+		
+		System.out.println("HashMap: " + hm);
+		
+		
+		// forEach method (Lambda Fxn)
+		
+		hm.forEach((key,value)->{
+			System.out.println(key+" -> "+value);
+		});
+		
+		
+		// To get single element (using Key)
+		System.out.println(hm.get("ABC"));
+		
+		
+	}
+}
