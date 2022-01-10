5748cdd2e88c779ffa44a6a2c6d9804a2f438637
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2022-01-11 00:04:58 +0530

    Create JDBCProgram3InsertFromUser.java

---
+++ JDBC Programs/JDBCProgram3InsertFromUser.java
@@ -0,0 +1,51 @@
+package jdbc;
+
+import java.sql.*;
+
+// here we are using BufferReader Class for taking input from the user
+import java.io.*;
+
+public class JDBCProgram3InsertFromUser {
+
+	public static void main(String[] args) {
+		
+		try {
+			 /* Step 1: Load the driver */
+	         Class.forName("com.mysql.jdbc.Driver");
+	         
+	         /* Step 2 : Create a connection */
+	         String url = "jdbc:mysql://localhost:3306/dbName";
+	         String username = "root";
+	         String password = "root";
+	         Connection con = DriverManager.getConnection(url,username,password);
+
+	         /* Step 3 : Create a Query : here we are creating the Insert Query */
+	         String q = "insert into table1(tName, tCity) values (?,?)";
+	         
+	         // to fire the Insert Query (dynamic query), use PreparedStatement 
+	         PreparedStatement ps = con.prepareStatement(q);
+	         
+	         // setting the values to query (here don't pass the tName tCity directly, use BufferReader to take input from the user)
+	         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	         
+	         System.out.println("Enter tName: ");
+	         String name = br.readLine();
+	         
+	         System.out.println("Enter tCity: ");
+	         String city = br.readLine();
+	         
+	         ps.setString(1,name);  // this is the tName (setString, bcoz the tName is of String type)
+	         ps.setString(2, city);  // this is tCity
+	         
+	         // here we are not getting the output (like we get in the SELECT Statement) so use executeUpdate(), and in executeUpdate dont use query q bcoz it was already used in the prepareStatement
+	         ps.executeUpdate();
+	         
+	         // Print message
+	         System.out.println("Inserted...");
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
