7db981fbc3a93b63d5bb5dc27ea70c5f8b3a7d39
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2022-01-23 00:29:43 +0530

    Create DaemonThread.java

---
+++ Multithreading/DaemonThread.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * Daemon Threads: is a thread that provides services to the user Thread.
+ * 
+ * i.e the life of the Daemon thread is same as the life of the user thread.
+ * 
+ * Various Methods: setDaemon(Boolean)  -- to make any thread: Daemon Thread or Service Provider Thread: eg.  thread.setDaemon(true);
+ * 					        public Boolean isDaemon()  -- to check whether a thread is Daemon or not.
+ * 
+ * Garbage Collector is best example of Daemon Thread.
+ * 
+ */
